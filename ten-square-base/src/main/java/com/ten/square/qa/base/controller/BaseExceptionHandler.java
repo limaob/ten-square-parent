4de92ce4a9ac2617b189bf11e0df2349c6f5ff69
limaob <502483164@qq.com>
2019-04-09 20:54:25 +0800

    添加qa模块

---
+++ ten-square-base/src/main/java/com/ten/square/qa/base/controller/BaseExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.ten.square.qa.base.controller;
+
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Description: base 模块统一异常处理
+ * @Author: limaob
+ * @CreateDate: 2019/3/26
+ */
+@ControllerAdvice
+public class BaseExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        e.printStackTrace();
+        return new Result(false, StatusCode.ERROR, e.getMessage());
+    }
+}
